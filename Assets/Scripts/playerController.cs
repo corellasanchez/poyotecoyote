c0f2647398b46ec5ad6c0f1af4c62b239b54afe9
Jhoel Gonzalez <jhoelgonzalez@Jhoels-MacBook-Pro.local>
2019-07-26 18:25:25 -0600

    touch and click movement

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,14 +6,13 @@ using UnityEngine.UI;
 public class playerController : MonoBehaviour
 {
     public float speed;
-
     private Rigidbody2D rb2d;
-
     private int count;
-
     public Text countText;
-
     public Text winText;
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
 
     void Start()
     {
@@ -23,14 +22,39 @@ public class playerController : MonoBehaviour
         winText.text =  "" ;
     }
 
+    void Update(){
+        if(Input.GetMouseButtonDown(0)){
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            // circle.transform.position = pointA * -1;
+            // outerCircle.transform.position = pointA * -1;
+            // circle.GetComponent<SpriteRenderer>().enabled = true;
+            // outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if(Input.GetMouseButton(0)){
+            touchStart = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }else{
+            touchStart = false;
+        }
+    }
+
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        if(touchStart){
+            // Vector2 offset = pointB - pointA;
+            Vector2 offset = pointA - pointB;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction * -1);
+            // circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y) * -1;
+        }else{
+            // circle.GetComponent<SpriteRenderer>().enabled = false;
+            // outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
 
-        rb2d.AddForce(movement * speed);
+    void moveCharacter(Vector2 direction){
+        rb2d.AddForce(direction * speed * Time.deltaTime);
     }
 
     public bool characterInInQuicksand;
